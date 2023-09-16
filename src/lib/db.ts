099007decd1f06016c76a310a1cb0bf327aa4241
Sandeep K <blackgen629@gmail.com>
2023-09-16 14:23:20 +0530

    updated prisma

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -6,12 +6,6 @@ declare global {
   var cachedPrisma: PrismaClient;
 }
 
-export let prisma: PrismaClient;
-if (process.env.NODE_ENV === "production") {
-  prisma = new PrismaClient();
-} else {
-  if (!global.cachedPrisma) {
-    global.cachedPrisma = new PrismaClient();
-  }
-  prisma = global.cachedPrisma;
-}
+export const prisma = new PrismaClient({
+  log: ['query', 'info', 'warn', 'error']
+});
